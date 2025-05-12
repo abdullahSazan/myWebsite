f4a364fd3931814dd937b11bd015cfd7a33f2039
Abdullah SAZAN <sazan1dev@gmail.com>
2025-05-12 23:19:02 +0300

    dosya yapısı oluşturuldu

---
+++ frontend/app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>başlık burası</h1>
+    </div>
+  );
+}
---
+++ frontend/app/projects/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function projects() {
+    return (
+        <div>project</div>
+    )
+}
+
+export default projects
